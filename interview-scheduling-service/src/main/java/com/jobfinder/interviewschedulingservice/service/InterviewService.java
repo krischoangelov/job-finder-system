241e75747a0b829c8c7a75f18ff64af6b3f44b1e
Kristiyan Angelov <kristianangelov2003@gmail.com>
2026-08-09 18:37:15 +0300

    feat: logging in InterviewScheduling Microservice

--- interview-scheduling-service/src/main/java/com/jobfinder/interviewschedulingservice/service/InterviewService.java
+++ interview-scheduling-service/src/main/java/com/jobfinder/interviewschedulingservice/service/InterviewService.java
@@ -9,6 +9,7 @@ import com.jobfinder.interviewschedulingservice.model.enums.InterviewStatus;
 import com.jobfinder.interviewschedulingservice.repository.InterviewRepository;
 import com.jobfinder.interviewschedulingservice.utils.Mapper;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.time.LocalDateTime;
@@ -17,6 +18,7 @@ import java.util.UUID;
 
 import static com.jobfinder.interviewschedulingservice.utils.Mapper.mapToInterviewResponse;
 
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class InterviewService {
@@ -41,33 +43,46 @@ public class InterviewService {
 
         Interview savedInterview = interviewRepository.save(interview);
 
+        log.info("Successfully created an interview with id={}", savedInterview.getId());
+
         return mapToInterviewResponse(savedInterview);
     }
 
     public InterviewResponse getInterviewById(UUID id) {
         Interview interview = interviewRepository.findById(id)
-                .orElseThrow(() ->
-                        new RuntimeException("Interview with the searched ID was not found")
-                );
+                .orElseThrow(() -> {
+
+                    log.warn("Interview not found with id={}", id);
+
+                    return new RuntimeException("Interview with this ID was not found");
+                });
+
+        log.info("Interview with id={} was found", interview.getId());
 
         return mapToInterviewResponse(interview);
     }
 
 
     public List<InterviewResponse> getInterviewsByRecruiterId(UUID recruiterId) {
-
-        return interviewRepository.findAllByRecruiterId(recruiterId)
+        List<InterviewResponse> interviews = interviewRepository.findAllByRecruiterId(recruiterId)
                 .stream()
                 .map(Mapper::mapToInterviewResponse)
                 .toList();
+
+        log.info("Found {} interviews for recruiterId={}", interviews.size(), recruiterId);
+
+        return interviews;
     }
 
     public List<InterviewResponse> getInterviewsByCandidateId(UUID candidateId) {
-
-        return interviewRepository.findAllByCandidateId(candidateId)
+        List<InterviewResponse> interviews = interviewRepository.findAllByCandidateId(candidateId)
                 .stream()
                 .map(Mapper::mapToInterviewResponse)
                 .toList();
+
+        log.info("Found {} interviews for candidateId={}", interviews.size(), candidateId);
+
+        return interviews;
     }
 
     public InterviewResponse updateInterview(
@@ -75,24 +90,23 @@ public class InterviewService {
             UpdateInterviewRequest request) {
 
         Interview interview = interviewRepository.findById(id)
-                .orElseThrow(() ->
-                        new RuntimeException("Interview with this ID was not found")
-                );
+                .orElseThrow(() -> {
+
+                    log.warn("Cannot update interview because id={} was not found", id);
+
+                    return new RuntimeException("Interview with this ID was not found");
+                });
 
         interview.setInterviewDate(request.getScheduledAt().toLocalDate());
-
         interview.setInterviewTime(request.getScheduledAt().toLocalTime());
-
         interview.setMeetingLink(request.getMeetingLink());
-
         interview.setLocation(request.getLocation());
-
         interview.setType(request.getType());
-
         interview.setUpdatedOn(LocalDateTime.now());
 
-        Interview updatedInterview =
-                interviewRepository.save(interview);
+        Interview updatedInterview = interviewRepository.save(interview);
+
+        log.info("Successfully updated interview with id={}", id);
 
         return mapToInterviewResponse(updatedInterview);
     }
@@ -100,15 +114,19 @@ public class InterviewService {
     public InterviewResponse confirmInterview(UUID id) {
 
         Interview interview = interviewRepository.findById(id)
-                .orElseThrow(() ->
-                        new RuntimeException("Interview with this ID was not found")
-                );
+                .orElseThrow(() -> {
+
+                    log.warn("Cannot confirm interview because id={} was not found", id);
+
+                    return new RuntimeException("Interview with this ID was not found");
+                });
 
         interview.setStatus(InterviewStatus.CONFIRMED);
         interview.setUpdatedOn(LocalDateTime.now());
 
-        Interview updatedInterview =
-                interviewRepository.save(interview);
+        Interview updatedInterview = interviewRepository.save(interview);
+
+        log.info("Successfully confirmed interview with id={}", id);
 
         return mapToInterviewResponse(updatedInterview);
     }
@@ -116,15 +134,19 @@ public class InterviewService {
     public InterviewResponse declineInterview(UUID id) {
 
         Interview interview = interviewRepository.findById(id)
-                .orElseThrow(() ->
-                        new RuntimeException("Interview with this ID was not found")
-                );
+                .orElseThrow(() -> {
+
+                    log.warn("Cannot decline interview because id={} was not found", id);
+
+                    return new RuntimeException("Interview with this ID was not found");
+                });
 
         interview.setStatus(InterviewStatus.DECLINED);
         interview.setUpdatedOn(LocalDateTime.now());
 
-        Interview updatedInterview =
-                interviewRepository.save(interview);
+        Interview updatedInterview = interviewRepository.save(interview);
+
+        log.info("Successfully declined interview with id={}", id);
 
         return mapToInterviewResponse(updatedInterview);
     }
@@ -132,15 +154,19 @@ public class InterviewService {
     public InterviewResponse cancelInterview(UUID id) {
 
         Interview interview = interviewRepository.findById(id)
-                .orElseThrow(() ->
-                        new RuntimeException("Interview with this ID was not found")
-                );
+                .orElseThrow(() -> {
+
+                    log.warn("Cannot cancel interview because id={} was not found", id);
+
+                    return new RuntimeException("Interview with this ID was not found");
+                });
 
         interview.setStatus(InterviewStatus.CANCELLED);
         interview.setUpdatedOn(LocalDateTime.now());
 
-        Interview updatedInterview =
-                interviewRepository.save(interview);
+        Interview updatedInterview = interviewRepository.save(interview);
+
+        log.info("Successfully canceled interview with id={}", id);
 
         return mapToInterviewResponse(updatedInterview);
     }
